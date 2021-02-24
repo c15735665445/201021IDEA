b4f7fd62b51e1e904e3a3926f5adda06c0c82c42
cmy15735665445 <c15735665445@aliyun.com>
2021-02-24 21:55:58 +0800

    add gn

---
+++ src/main/java/com/atguigu/Branch.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class Branch {
+    public static void main(String[] args) {
+        System.out.println("增加新的功能 ： 分支");
+
+        System.out.println("徒弟将分支上传到 +远端+ ，师傅pull将分支合并");
+    }
+}
