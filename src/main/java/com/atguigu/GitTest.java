03622479927aeeb981c12ba9afed57179b9bdcf8
c15735665445 <1454208157@qq.com>
2021-02-24 20:41:41 +0800

    IDEA提交

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("145");
+        System.out.println("145");
+        System.out.println("145");
+    }
+}
